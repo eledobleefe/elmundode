b3d0126754eaa49ce57fd09e0084e58de8a36ace
eledobleefe <eledobleefe@gmail.com>
2020-06-04 00:45:21 +0200

    Actualización BBDD
    Añadimos resticciones UNIQUE

--- sql/bbdd.sql
+++ sql/bbdd.sql
@@ -11,12 +11,11 @@ CREATE TABLE `elmundode`.`bebe` (
     `horaNacimiento` TIME NOT NULL,
     `hospitalNacimiento` VARCHAR (50),
     `lugarNacimiento` VARCHAR (50),
-    `pesoBebe` FLOAT NOT NULL,
-    `alturaBebe` FLOAT NOT NULL,
-    `cabezaBebe` FLOAT NOT NULL,
+    `ciudadNacimiento` VARCHAR (50),
     `grupoSanguineo` ENUM('0 -', '0 +', 'A -', 'A +', 'B -', 'B +', 'AB -', 'AB +'),
     `imgNacimiento` VARCHAR (50),
-    `dedicatoriaBebe` VARCHAR (200) NOT NULL
+    `dedicatoriaBebe` VARCHAR (200) NOT NULL,
+    CONSTRAINT BebeUnico UNIQUE (nombreBebe, apellidosBebe, fechaNacimiento, horaNacimiento, lugarNacimiento)
 ) ENGINE = INNODB;
 
 
@@ -29,7 +28,8 @@ CREATE TABLE `elmundode`.`usuario` (
     `pass` VARCHAR (32) NOT NULL,
     -- Ponemos 32 porque es el número de caracteres al pasar la contraseña md5
     `rol` ENUM('creador', 'visitante'),
-    `idBebe` INT NOT NULL
+    `idBebe` INT NOT NULL,
+    CONSTRAINT UsuarioUnico UNIQUE (email, pass)
 ) ENGINE = INNODB;
 
 ALTER TABLE `elmundode`.`usuario`
@@ -43,19 +43,20 @@ CREATE TABLE `elmundode`.`progenitor` (
     `fechaNProgenitor`  DATE NOT NULL,
     `lugarNProgenitor` VARCHAR (50) NOT NULL,
     `descripcionProgenitor`VARCHAR (50) NOT NULL,
-    `idBebe` INT NOT NULL
+    `idBebe` INT NOT NULL,
+    CONSTRAINT ProgenitorUnico UNIQUE (nombreProgenitor, apellidosProgenitor, fechaNProgenitor, lugarNProgenitor)
 ) ENGINE = INNODB;
 
 ALTER TABLE `elmundode`.`progenitor`
 ADD FOREIGN KEY (`idBebe`) REFERENCES `elmundode`.`bebe`(`idBebe` );
 
 CREATE TABLE `elmundode`.`embarazo` (
+    `idBebe` INT NOT NULL,
+    `idProgenitor` INT NOT NULL,
     `semanasEmbarazo` INT NOT NULL,
     `diasEmbarazo` INT NOT NULL,
     `kgAumento` FLOAT NOT NULL,
     `fechaNoticia` DATE NOT NULL,
-    `idBebe` INT NOT NULL,
-    `idProgenitor` INT NOT NULL,
     CONSTRAINT fkembarazo1 FOREIGN KEY (`idBebe`) REFERENCES `elmundode`.`bebe`(`idBebe` ),
     CONSTRAINT fkembarazo2 FOREIGN KEY (`idProgenitor`) REFERENCES `elmundode`.`progenitor`(`idProgenitor` )
 ) ENGINE = INNODB;
@@ -66,11 +67,11 @@ ADD PRIMARY KEY (`idBebe`, `idProgenitor`);
 
 
 CREATE TABLE `elmundode`.`crecimiento` (
+    `idBebe` INT NOT NULL,
     `fechaDatos` DATE NOT NULL,
     `altura` FLOAT NOT NULL,
     `peso` FLOAT NOT NULL,
     `cabeza` FLOAT NOT NULL,
-    `idBebe` INT NOT NULL,
     CONSTRAINT fkcrecimiento FOREIGN KEY (`idBebe`) REFERENCES `elmundode`.`bebe`(`idBebe` )
 ) ENGINE = INNODB;
 
@@ -79,10 +80,10 @@ ADD PRIMARY KEY (`idBebe`, `fechaDatos`);
 
 
 CREATE TABLE `elmundode`.`dentadura` (
+    `idBebe` INT NOT NULL,
     `fechaDiente` DATE NOT NULL,
     `ordenDiente` ENUM ('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'),
     `nombreDiente` ENUM ('Incisivo central izquierdo superior','Incisivo central derecho superior','Incisivo lateral izquierdo superior','Incisivo lateral derecho superior','Canino izquierdo superior','Canino derecho superior','Primer molar izquierdo superior','Primer molar derecho superior','Segundo molar izquierdo superior','Segundo molar derecho superior','Incisivo central izquierdo inferior','Incisivo central derecho inferior','Incisivo lateral izquierdo inferior','Incisivo lateral derecho inferior','Canino izquierdo inferior','Canino derecho inferior','Primer molar izquierdo inferior','Primer molar derecho inferior','Segundo molar izquierdo inferior','Segundo molar derecho inferior'),
-     `idBebe` INT NOT NULL,
      CONSTRAINT fkdentadura FOREIGN KEY (`idBebe`) REFERENCES `elmundode`.`bebe`(`idBebe` )
 ) ENGINE = INNODB;
 
@@ -91,13 +92,13 @@ ADD PRIMARY KEY (`idBebe`, `nombreDiente`);
 
 
 CREATE TABLE `elmundode`.`anecdota` (
+    `idBebe` INT NOT NULL,
     `fechaAnecdota` DATE NOT NULL,
     `descripcionAnecdota` VARCHAR (50) NOT NULL,
     `nombreAnecdota` VARCHAR (50),
     `lugarAnecdota` VARCHAR (50),
     `extraAnecdota` VARCHAR (200),
     `tipoExtra` ENUM ('link', 'img'),
-    `idBebe` INT NOT NULL,
     CONSTRAINT fkanecdota FOREIGN KEY (`idBebe`) REFERENCES `elmundode`.`bebe`(`idBebe` )
 ) ENGINE = INNODB;
 
