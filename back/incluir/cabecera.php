dea8d6689788e4b635d8ea8ca487c1b8e198bcd0
eledobleefe <65417731+eledobleefe@users.noreply.github.com>
2020-06-22 02:11:23 +0200

    Piezas HTML
    Creamos el <head>, el <footer> y los <script> para incluir en las páginas y centrar posibles cambios

---
+++ back/incluir/cabecera.php
@@ -0,0 +1,16 @@
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Fuentes google fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet"> 
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="estilos/bootstrap.css">
+<!-- Nuestro CSS -->
+<link rel="stylesheet" href="estilos/elmundode.css">
+<!-- Iconos -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<!--Favicon-->
+<link rel="icon" type="image/png" href="img/elmundode/favicon.png" sizes="32x32">
+<title>El mundo de</title>
+</head>
---
+++ back/incluir/footer.php
@@ -0,0 +1,23 @@
+<!--FOOTER-->	
+	<footer class="container-fluid bg-dark text-white textofooter py-4">
+		<div class="container">
+			<div class="row justify-content-between">
+				<div class="col-sm-12 col-md-1">
+					<p>Logo</p>
+				</div>
+				<div class="col-sm-12 col-md-3">
+					<p>Proyecto de Laura Fernández Fernández para C.S. Desarrollo de aplicaciones web</p>
+				</div>
+				<div class="col-sm-12 col-md-3">
+					<ul>
+						<li><a href="#" alt="Política de cookies">Política de cookies</a></li>
+						<li><a href="#" alt="Política de privacidad">Política de privacidad</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-12 col-md-1">
+					<a href="#" alt="Perfil de LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--Fin del footer-->
